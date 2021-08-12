dc9f9378b3ded5faf28f2f92645daec0a3ca392e
Andrey Ermolchik <frutizzzzz@gmail.com>
2021-08-12 12:42:00 +0300

    lesson_11

--- JavaUIAutomation/src/test/java/NewProject/Main.java
+++ JavaUIAutomation/src/test/java/NewProject/Main.java
@@ -16,25 +16,34 @@ public class Main {
         System.setProperty("webdriver.chrome.driver", "E://webdriver/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
-        driver.get("https://diary.ru/user/login");
+       try {
+           driver.get("https://crm.geekbrains.space/user/login");
 
-        driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
-        WebDriverWait element = (new WebDriverWait(driver,20));
+           driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+           WebDriverWait element = (new WebDriverWait(driver, 20));
 
-        WebElement inputlogin = driver.findElement(By.name("LoginForm[username]"));
-        inputlogin.click();
-        inputlogin.sendKeys("franfrukt");
+           WebElement inputlogin = driver.findElement(By.xpath("//input[@id='prependedInput']"));
+           inputlogin.click();
+           inputlogin.sendKeys("Applanatest1");
 
-        WebElement inputpassword = driver.findElement(By.name("LoginForm[password]"));
-        inputpassword.click();
-        inputpassword.sendKeys("qwerty");
+           WebElement inputpassword = driver.findElement(By.xpath("//input[@id='prependedInput2']"));
+           inputpassword.click();
+           inputpassword.sendKeys("Student2020!");
 
-        WebElement captcha = driver.findElement(By.xpath("//div[@class=’recaptcha-checkbox-border’]"));
-        captcha.click();
+           WebElement button = driver.findElement(By.xpath("//button[@id='_submit']"));
+           button.click();
 
-        WebElement button = driver.findElement(By.name("login-button"));
-        button.click();
+           //new WebDriverWait(driver,15);
+
+           //WebElement myproject = driver.findElement(By.cssSelector("div.dropdown:nth-child(3) .single:nth-child(4) .title"));
+           //myproject.click();
+
+       } catch (Exception e) {
+           e.printStackTrace();
+       }
+
 
     }
-}
+    }
+//    ("//div[@class=’recaptcha-checkbox-border’]")
 
