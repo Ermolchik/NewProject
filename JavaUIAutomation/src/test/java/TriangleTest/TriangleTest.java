79fba692c2099283bbfcff20feafe7311aa9e8c3
Andrey Ermolchik <frutizzzzz@gmail.com>
2021-08-12 16:29:11 +0300

    lesson_12

--- JavaUIAutomation/src/test/java/TriangleTest/TriangleTest.java
+++ JavaUIAutomation/src/test/java/TriangleTest/TriangleTest.java
@@ -3,9 +3,10 @@ package TriangleTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TriangleTest {
-    public static void main( String[] args )
-    {
+    public static void main(String[] args) {
         double a;
 
         a = triangleArea(10, 12, 10);
@@ -14,21 +15,41 @@ public class TriangleTest {
         a = triangleArea(3, 4, 5);
         System.out.println("Площадь треугольника со сторонами 3,4,5 будет равна:" + a);
 
-        a = triangleArea(9, 8, 9);
-        System.out.println("Площадь треугольника со сторонами 9,9,9 будет равна:" + a );
+        a = triangleArea(9, 9, 9);
+        System.out.println("Площадь треугольника со сторонами 9,9,9 будет равна:" + a);
 
+        CorrectTest();
+        InCorrectTest();
     }
 
-    public static double triangleArea( int a, int b, int c )
-    {
+    public static double triangleArea(int a, int b, int c) {
         double pp = (a + b + c) / 2.0;
         return Math.sqrt(pp * (pp - a) * (pp - b) * (pp - c));
     }
+
     @Test
     @DisplayName("Площадь треугольнка")
-    public static void TriangleTest(double a,double b){
+    public static void CorrectTest() {
+       try {
+           assertEquals(triangleArea(10, 12, 10), 48.0);
+           assertEquals(triangleArea(3, 4, 5), 6.0);
+           assertEquals(triangleArea(9, 9, 9), 35.074028853269766);
+           System.out.println("Тест: Площадь треугольника, прошёл успешно!");
+       } catch (Exception e) {
+           e.printStackTrace();
+       }
+    }
 
-
+    @Test
+    @DisplayName("Не корректные данные")
+    public static void InCorrectTest() {
+       try {
+           assertEquals(triangleArea(11, 12, 10), 48.0);
+           assertEquals(triangleArea(3, 4, 6), 6.0);
+           assertEquals(triangleArea(9, 8, 9), 35.074029);
+       } catch (Exception e) {
+            e.printStackTrace();
+       }
     }
 
 }
